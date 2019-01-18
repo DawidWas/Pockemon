78a26ffca082f28ef103be8798005e8031670890
730004251 <dawidwas@macbook-pro-dawid.local>
2019-01-18 15:55:31 +0100

    Pock location

---
+++ Pockemon/Pockemons.swift
@@ -0,0 +1,31 @@
+//
+//  Pockemons.swift
+//  Pockemon
+//
+//  Created by Dawid Was on 18/01/2019.
+//  Copyright © 2019 Dawid Was. All rights reserved.
+//
+
+import UIKit
+
+class Pockemons: NSObject {
+    
+    var latitude:Double?
+    var longitude:Double?
+    var image:String?
+    var name:String?
+    var des:String?
+    var power:Double?
+    var isCatch:bool?
+    init(latitude:Double,longitude:Double,image:String,name:String,des:String,power:Double) {
+        
+        self.latitude=latitude
+        self.longitude=longitude
+        self.image=image
+        self.name=name
+        self.des=des
+        self.power=power
+        self.isCatch=false
+        
+    }
+}
--- Pockemon/ViewController.swift
+++ Pockemon/ViewController.swift
@@ -12,8 +12,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     var mapView: GMSMapView!
     let locationManager = CLLocationManager()
+    var listPockemon = [Pockemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        LoadPockemons()
         
         let camera = GMSCameraPosition.camera(withLatitude: 43, longitude: -77, zoom: 10)
         mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
@@ -35,17 +38,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         oldLocation = manager.location!.coordinate
+        self.mapView.clear()
         
+        //mylocation
         let markerMe = GMSMarker()
         markerMe.position = CLLocationCoordinate2D(latitude: oldLocation.latitude, longitude: oldLocation.longitude )
         markerMe.title = "Me"
         markerMe.snippet = "Here is my location"
         markerMe.icon = UIImage(named:"mario")
         markerMe.map = self.mapView
+        
+        for pockemon in listPockemon{
+            let markerPockemon = GMSMarker()
+            markerPockemon.position = CLLocationCoordinate2D(latitude: pockemon.latitude!, longitude: pockemon.longitude! )
+            markerPockemon.title = pockemon.name!
+            markerPockemon.snippet = "\(pockemon.des!), power \(pockemon.power!)"
+            markerPockemon.icon = UIImage(named:pockemon.image!)
+            markerPockemon.map = self.mapView
+        }
+        
+        
         let camera = GMSCameraPosition.camera(withLatitude: oldLocation.latitude, longitude: oldLocation.longitude, zoom: 15)
         self.mapView.camera = camera
 
         
+    }
+    
+    func LoadPockemons(){
+        self.listPockemon.append(Pockemon(latitude: 37.7789994893035, longitude: -122.401846647263, image: "charmander", name: "Charmander", des: "Fire pockemon", power: 55))
+        self.listPockemon.append(Pockemon(latitude: 37.7949994893035, longitude: -122.410446647263, image: "bulbasaur", name: "Bulbasaur", des: "Nature pockemon", power: 90))
+        self.listPockemon.append(Pockemon(latitude: 37.7816994893035, longitude: -122.412246647263, image: "squirtle", name: "Squirtle", des: "Water pockemon", power: 33))
     }
 
 }
